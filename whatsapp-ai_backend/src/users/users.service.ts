c70576e716090df6e57a35ace9132259a58da693
CasianLW <ciorbacasian@gmail.com>
2024-09-23 19:09:08 +0200

    service for users, begining of dev

--- whatsapp-ai_backend/src/users/users.service.ts
+++ whatsapp-ai_backend/src/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema.ts/user.schema.ts';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(user: User): Promise<User> {
+    const createdUser = new this.userModel(user);
+    return createdUser.save();
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+
+  async updateCredits(userId: string, credits: number): Promise<User> {
+    return this.userModel
+      .findByIdAndUpdate(
+        userId,
+        { $inc: { credits } }, // Increment or decrement credits
+        { new: true },
+      )
+      .exec();
+  }
+}
