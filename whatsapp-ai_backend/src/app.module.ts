361c9f9ae0b566533c9f59dddf49c0137e70f8ba
CasianLW <ciorbacasian@gmail.com>
2024-09-23 19:07:45 +0200

    added mongodb URI connection

--- whatsapp-ai_backend/src/app.module.ts
+++ whatsapp-ai_backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersService } from './users/users.service';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), , UsersModule],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
