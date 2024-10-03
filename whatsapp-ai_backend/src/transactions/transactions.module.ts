34034cb16d456837ad9d19c1bdb5f1ed9ea3f73b
CasianLW <ciorbacasian@gmail.com>
2024-10-03 22:23:05 +0200

    created transactions module & integrated mongoose module in it

---
+++ whatsapp-ai_backend/src/transactions/transactions.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Transaction, TransactionSchema } from './schemas/transaction.schema';
+import { TransactionsService } from './transactions.service';
+import { TransactionsController } from './transactions.controller';
+import { UsersModule } from '../users/users.module';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Transaction.name, schema: TransactionSchema },
+    ]),
+    UsersModule,
+  ],
+  controllers: [TransactionsController],
+  providers: [TransactionsService],
+  exports: [TransactionsService],
+})
+export class TransactionsModule {}
