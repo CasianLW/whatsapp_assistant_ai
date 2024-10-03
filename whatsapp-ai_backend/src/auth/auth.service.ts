308ec3530c8ea26669b6ce88a02ed8259cd49510
CasianLW <ciorbacasian@gmail.com>
2024-10-03 16:18:50 +0200

    services for auth ( login, vaidate user , hash pass)

--- whatsapp-ai_backend/src/auth/auth.service.ts
+++ whatsapp-ai_backend/src/auth/auth.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
+import { UsersService } from '../users/users.service';
+import { JwtPayload } from './jwt-payoad.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.usersService.findByEmail(email); // Implement findByEmail in UsersService
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Compare hashed password
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload: JwtPayload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(password, salt);
+  }
+}
