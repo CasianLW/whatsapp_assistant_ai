a682004434e0d5be443004cb32f644d381e39807
CasianLW <ciorbacasian@gmail.com>
2024-10-03 17:29:57 +0200

    created module with exports & mongodb table def for specialcodes

--- whatsapp-ai_backend/src/specialcodes/specialcodes.module.ts
+++ whatsapp-ai_backend/src/specialcodes/specialcodes.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SpecialCodesService } from './specialcodes.service';
+import { SpecialCodesController } from './specialcodes.controller';
+import { SpecialCodeSchema } from './schemas/specialcode.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'SpecialCode', schema: SpecialCodeSchema },
+    ]), // Register the special code model
+  ],
+  providers: [SpecialCodesService],
+  controllers: [SpecialCodesController],
+  exports: [SpecialCodesService],
+})
 export class SpecialcodesModule {}
