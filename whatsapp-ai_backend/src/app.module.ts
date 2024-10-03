edd28f9f4b3d68a5d4bd08eab1553bf420484a18
CasianLW <ciorbacasian@gmail.com>
2024-10-03 17:02:11 +0200

    correction of the module imports

--- whatsapp-ai_backend/src/app.module.ts
+++ whatsapp-ai_backend/src/app.module.ts
@@ -1,18 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersService } from './users/users.service';
-import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
-import { SpecialcodesController } from './specialcodes/specialcodes.controller';
-import { SpecialcodesService } from './specialcodes/specialcodes.service';
 import { SpecialcodesModule } from './specialcodes/specialcodes.module';
-import { PromocodesController } from './promocodes/promocodes.controller';
-import { PromocodesService } from './promocodes/promocodes.service';
-import { PromocodesModule } from './promocodes/promocodes.module';
+import { PromoCodesModule } from './promocodes/promocodes.module';
 
 @Module({
   imports: [
@@ -21,9 +15,9 @@ import { PromocodesModule } from './promocodes/promocodes.module';
     UsersModule,
     AuthModule,
     SpecialcodesModule,
-    PromocodesModule,
+    PromoCodesModule,
   ],
-  controllers: [AppController, SpecialcodesController, PromocodesController],
-  providers: [AppService, SpecialcodesService, PromocodesService],
+  controllers: [AppController], // Remove SpecialcodesController and PromoCodesController
+  providers: [AppService], // Remove SpecialcodesService and PromoCodesService
 })
 export class AppModule {}
