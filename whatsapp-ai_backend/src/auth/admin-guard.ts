09fea26ad886bf56c5d8ba1db990e2bcc7b53c59
CasianLW <ciorbacasian@gmail.com>
2024-10-03 18:13:24 +0200

    fixed guard

--- whatsapp-ai_backend/src/auth/admin-guard.ts
+++ whatsapp-ai_backend/src/auth/admin-guard.ts
@@ -4,23 +4,29 @@ import {
   ExecutionContext,
   ForbiddenException,
 } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
-import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Injectable()
-export class AdminGuard extends JwtAuthGuard implements CanActivate {
-  constructor(private reflector: Reflector) {
-    super();
-  }
-
+export class AdminGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
     const user = request.user;
 
-    if (user && user.role === 'admin') {
+    // console.log('AdminGuard - User:', user); // Log the user object for debugging
+
+    if (!user) {
+      //   console.log(
+      //     'AdminGuard - Unauthorized Access Attempt: No user found in request.',
+      //   );
+      throw new ForbiddenException('User is not authenticated');
+    }
+
+    // Check if the user exists and has the role of 'admin'
+    if (user.role === 'admin') {
+      //   console.log('AdminGuard - User is admin');
       return true;
     }
 
+    // console.log('AdminGuard - Unauthorized Access Attempt by:', user);
     throw new ForbiddenException(
       'You do not have permissions to access this resource',
     );
