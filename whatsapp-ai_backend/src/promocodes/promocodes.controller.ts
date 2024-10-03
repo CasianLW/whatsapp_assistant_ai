f8adb4fd84e5c19c96b971dc60224901d587f0f8
CasianLW <ciorbacasian@gmail.com>
2024-10-03 17:29:15 +0200

    promo codes controller done; with admin and auth guards

--- whatsapp-ai_backend/src/promocodes/promocodes.controller.ts
+++ whatsapp-ai_backend/src/promocodes/promocodes.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  UseGuards,
+  Delete,
+  Request,
+} from '@nestjs/common';
+import { PromoCodesService } from './promocodes.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { AdminGuard } from 'src/auth/admin-guard';
 
 @Controller('promocodes')
-export class PromocodesController {}
+export class PromoCodesController {
+  constructor(private readonly promoCodesService: PromoCodesService) {}
+
+  @UseGuards(JwtAuthGuard, AdminGuard)
+  @Post('create')
+  async createPromoCode(
+    @Body('code') code: string,
+    @Body('discount') discount: number,
+    @Body('maxUsage') maxUsage: number,
+    @Body('expiresAt') expiresAt: Date,
+  ) {
+    return this.promoCodesService.createPromoCode(
+      code,
+      discount,
+      maxUsage,
+      expiresAt,
+    );
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('redeem')
+  async redeemPromoCode(@Body('code') code: string, @Request() req) {
+    const userId = req.user.userId; // Extract userId from the request object
+    return this.promoCodesService.redeemPromoCode(userId, code);
+  }
+
+  @UseGuards(JwtAuthGuard, AdminGuard)
+  @Delete(':code')
+  async deletePromoCode(@Param('code') code: string) {
+    return this.promoCodesService.deletePromoCode(code);
+  }
+
+  @UseGuards(JwtAuthGuard, AdminGuard)
+  @Get()
+  async getAllPromoCodes() {
+    return this.promoCodesService.getAllPromoCodes();
+  }
+}
