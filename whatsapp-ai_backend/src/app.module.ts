5b45e29b49502aaef1d75eb13602a909015bd216
CasianLW <ciorbacasian@gmail.com>
2024-10-03 16:16:56 +0200

    added dotenv & mongodb config

--- whatsapp-ai_backend/src/app.module.ts
+++ whatsapp-ai_backend/src/app.module.ts
@@ -6,10 +6,16 @@ import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), , UsersModule, AuthModule],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    UsersModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
