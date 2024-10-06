fd847cfa521b4bc10986d9c5272fc182ed1d1821
CasianLW <ciorbacasian@gmail.com>
2024-10-06 18:57:39 +0200

    adapted schema to newer requirements

--- whatsapp-ai_backend/src/bot/schemas/bot-config.schema.ts
+++ whatsapp-ai_backend/src/bot/schemas/bot-config.schema.ts
@@ -1,19 +1,28 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
+export type BotContext = 'professional' | 'personal' | 'humor' | 'assistant';
+export type BotType = 'openai' | 'predefined';
+
 @Schema()
 export class BotConfig extends Document {
   @Prop({ required: true })
   userId: string; // User ID linked to the bot config
 
-  @Prop({ default: 'default' })
-  context: string; // Bot context (e.g., "professional", "personal", "humor")
+  @Prop({ type: String, default: 'humor' })
+  context: BotContext; // Bot context (e.g., "professional", "personal", "humor", "assistant")
 
-  @Prop({ default: 'default' })
+  @Prop({ type: String, default: 'openai' })
+  botType: BotType; // Bot type ("openai" for AI-generated or "predefined" for static responses)
+
+  @Prop({ type: String, default: 'Francais' })
   language: string; // Language preference (if any)
 
-  @Prop({ default: [] })
+  @Prop({ type: [String], default: [] })
   customResponses: string[]; // Array of custom responses for specific queries
+
+  @Prop({ type: Object, default: {} })
+  openAiConfig: { apiKey: string; model: string }; // Configuration specific to OpenAI (apiKey, model)
 
   @Prop({ default: Date.now })
   createdAt: Date;
