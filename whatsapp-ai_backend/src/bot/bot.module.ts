867436523b588e8a2e32771bf60b08458da4646d
CasianLW <ciorbacasian@gmail.com>
2024-10-06 18:58:40 +0200

    imported openai module to bot, so we can use services

--- whatsapp-ai_backend/src/bot/bot.module.ts
+++ whatsapp-ai_backend/src/bot/bot.module.ts
@@ -3,12 +3,14 @@ import { BotController } from './bot.controller';
 import { BotService } from './bot.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BotConfig, BotConfigSchema } from './schemas/bot-config.schema';
+import { OpenaiModule } from 'src/openai/openai.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: BotConfig.name, schema: BotConfigSchema },
     ]),
+    OpenaiModule,
   ],
   controllers: [BotController],
   providers: [BotService],
